776f87deea0a6264cb47b8a1df9bc40a74ae6d8d
Yi leng Yao <yilengyao@gmail.com>
2022-12-25 13:09:50 -0800

    create music store table

--- GraphQL-MusicStore-Maven/src/main/resources/db/migration/V1__Initial.sql
+++ GraphQL-MusicStore-Maven/src/main/resources/db/migration/V1__Initial.sql
@@ -6,3 +6,34 @@ CREATE TABLE user (
 );
 
 insert into user (name, email) values ('john doe', 'doe@gmail.com');
+
+CREATE TABLE track (
+    track_id BIGINT NOT NULL AUTO_INCREMENT,
+    movie_title varchar(255) NOT NULL,
+    track_title varchar(255) NOT NULL,
+    tempo_name ENUM('fast', 'medium', 'slow') NOT NULL,
+    tempo_bpm INT NOT NULL,
+    composer_id BIGINT,
+    inventory INT,
+    created DATE,
+    PRIMARY KEY (track_id)
+);
+
+CREATE TABLE composers (
+    composer_id BIGINT NOT NULL,
+    composer_name varchar(255) NOT NULL,
+    PRIMARY KEY (composer_id)
+);
+
+CREATE TABLE instruments (
+    instrument_id BIGINT,
+    instrument_group varchar(255),
+    instrument_name varchar(255),
+    PRIMARY KEY (instrument_id)
+);
+
+CREATE TABLE track_instruments (
+    track_id BIGINT NOT NULL,
+    instrument_id BIGINT NOT NULL,
+    PRIMARY KEY (track_id, instrument_id)
+);
